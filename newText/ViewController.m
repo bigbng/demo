5b0646e67aa5dfd8bdbe93b3bc4e0fc538bcf5d1
Apple <favoshuai@gmail.com>
2018-03-06 10:37:29 +0800

    firstCommit

---
+++ newText/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  newText
+//
+//  Created by Apple on 2018/3/6.
+//  Copyright © 2018年 Apple. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    // 测试信息
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
